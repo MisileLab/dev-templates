5a13b909590219e013cd5855026687416e783476
misilelab <misileminecord@gmail.com>
2024-12-11 10:41:42 +0900

    chore: update nixpkgs
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- flake.nix
+++ flake.nix
@@ -95,7 +95,7 @@
                 --experimental-features 'nix-command flakes' \
                 flake init \
                 --template \
-                "github:the-nix-way/dev-templates#''${TEMPLATE}"
+                "github:misilelab/dev-templates#''${TEMPLATE}"
             '';
           };
         }
