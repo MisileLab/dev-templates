a73cdef68454d75b8339e4a75802398c7a9c47fb
misilelab <misileminecord@gmail.com>
2025-06-02 13:21:57 +0900

    chore: update lockfile
    Signed-off-by: misilelab <misileminecord@gmail.com>

--- rust-toolchain/flake.nix
+++ rust-toolchain/flake.nix
@@ -2,9 +2,9 @@
   description = "A Nix-flake-based Rust development environment";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixpkgs.url = "github:nixos/nixpkgs";
     fenix = {
-      url = "https://flakehub.com/f/nix-community/fenix/0.1";
+      url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
